a29780eae25f0691fca609fd8f2a061cdd42bbeb
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-07-12 23:25:00 -0500

    Thunk pattern -> liner Components yay 👍
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector, useDispatch} from 'react-redux';
-import { uiActions } from './store/indexRedux';
+import {sendCartData} from './store/cart-slice'; 
 import Notification from './components/UI/Notification'; 
 
 let isInitial = true; 
@@ -15,53 +15,11 @@ function App() {
   const notification = useSelector(state => state.ui.notification); 
 
   useEffect(()=>{
-    const fetchDataBase = async () => {
-      //INIT
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'sending...',
-          message: 'Sending cart data'
-        })
-      ); 
-      const response = await fetch(
-        'https://react-fire-50c82-default-rtdb.firebaseio.com/products.json',
-        {
-          method: 'PUT', 
-          //PUT:  We also store data on Firebase -> but the difference to post is that
-          // ** The new data will not be added in a list of data, but it will override existing data 
-          body: JSON.stringify(cart),
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        }
-      );
-      if(!response.ok){
-        throw new Error('sending cart data failed'); 
-      }; 
-      //SUCCESS
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'success',
-          message: 'Sending cart data successfully'
-        })
-      ); 
-    };
-    if(isInitial){
-      isInitial = false; 
-      return
-    }
-    fetchDataBase().catch(error => {
-      //ERROR
-        dispatch(
-          uiActions.showNotification({
-            status: 'error',
-            title: 'Error',
-            message: 'Sending cart data failed'
-          })
-        ); 
-    }); 
+   if (isInitial) {
+     isInitial = false; 
+     return;
+   }; 
+   dispatch(sendCartData(cart))
   },[cart, dispatch]); 
   return (
     <React.Fragment>
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from './indexRedux'; 
 
 const cartInitialState = {
   items: [],
@@ -40,5 +41,55 @@ const cartSlice = createSlice({
     },
   }
 });
+
+//Custom action creator Function----------------------------------------------------------------
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+        uiActions.showNotification({
+        status: 'pending',
+        title: 'sending...',
+        message: 'Sending cart data'
+      })
+    ); 
+    const sendRequest = async () => {
+      const response = await fetch(
+        'https://react-fire-50c82-default-rtdb.firebaseio.com/products.json',
+        {
+          method: 'PUT', 
+           
+          body: JSON.stringify(cart),
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+      if(!response.ok){
+        throw new Error('sending cart data failed'); 
+      }; 
+    };
+    try {
+      await sendRequest();
+      
+      //SUCCESS
+      dispatch(
+        uiActions.showNotification({
+          status: 'success',
+          title: 'success',
+          message: 'Sending cart data successfully'
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: 'error',
+          title: 'Error',
+          message: 'Sending cart data failed'
+        })
+      ); 
+    }
+  };
+}; 
 
 export default cartSlice; 
